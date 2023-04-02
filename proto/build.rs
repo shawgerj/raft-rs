af809f8425a826cd2e6de9b256f809553a47f74c
John Shawger <shawgerj@cs.wisc.edu>
2023-04-02 10:32:44 -0500

    fixed rust-protobuf and updated dependency

--- proto/build.rs
+++ proto/build.rs
@@ -1,7 +1,7 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
 use protobuf_build::Builder;
-
+  
 fn main() {
     let base = std::env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| ".".to_string());
     Builder::new()
